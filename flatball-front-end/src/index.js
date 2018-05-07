eb95401768cd6a32defdd8a385991f21d034fa62
JMoloughney <github email address>
2018-05-07 15:38:30 -0400

    built basic frontend template

--- flatball-front-end/src/index.js
+++ flatball-front-end/src/index.js
@@ -1,14 +1,84 @@
 document.addEventListener('DOMContentLoaded', function(){
+  newGame() // calling on new game function that sets up an object for a new game to be
+  //played when page loads
 
-// fetch("http://localhost:3000/api/v1/games")
-//   .then(res => res.json())
-//   .then(json => {
-//     new Game(json).render()
-//   })
+  // Main Page HTML Containers/Set-up Constants //
+  const appBody = document.querySelector('body')
+  const mainPageDiv = document.createElement('div')
+  mainPageDiv.setAttribute('id', 'main-page-div')
+  const welcomeMessage = document.createElement('h1')
+  welcomeMessage.setAttribute('id','main-welcome')
+  welcomeMessage.innerHTML = "Welcome to FlatBall: <br> Where Anybody Can PLAY BALL!"
+  mainPageDiv.append(welcomeMessage)
+  appBody.append(mainPageDiv)
+  const batterControls = document.getElementById('')
+  const pitcherControls = document.getElementById('')
+
+  //Play Ball Button
+  const newGameButtonDiv = document.createElement('div')
+  newGameButtonDiv.setAttribute('id', 'play-game-container')
+  mainPageDiv.append(newGameButtonDiv)
+  const newGameButton = document.createElement('button')
+  newGameButton.setAttribute('id', 'new-game-button')
+  newGameButton.innerText = 'Play Ball!'
+  newGameButtonDiv.append(newGameButton)
+  // end //
+
+
+  //Batter Controls//
+  const battersControlsContainer = document.createElement('div')
+  battersControlsContainer.setAttribute('id', 'batter-controls')
+  const bPower = document.createElement('button')
+  bPower.setAttribute('id', 'power-hit')
+  bPower.innerText = 'Power Hit'
+  const bHit = document.createElement('button')
+  bHit.setAttribute('id', 'contact-hit')
+  bHit.innerText = 'Hit for Contact'
+  battersControlsContainer.append(bHit, bPower)
+  //end//
+
+  //Pitching Controls//
+  const pitchersControlsContainer = document.createElement('div')
+  pitchersControlsContainer.setAttribute('id', 'pitcher-controls')
+  const pFastball = document.createElement('button')
+  pFastball.setAttribute('id', 'fastball')
+  pFastball.innerText = 'Fastball'
+  const pSpecial = document.createElement('button')
+  pSpecial.setAttribute('id', 'special-pitch')
+  pSpecial.innerText = 'Special Pitch'
+  pitchersControlsContainer.append(pFastball, pSpecial)
+  //end//
+
+
+  //Instructions on How to Play
+  const instructionsDiv = document.createElement('div')
+  instructionsDiv.setAttribute('id','instructions')
+  instructionsDiv.innerHTML = "<h2> How To Play: </h2><ul><h4>Batting</h4><li>Step 1</li></ul><br><br><ul><h4>Pitching</h4><li>Step 1</li></ul>"
+  mainPageDiv.append(instructionsDiv)
+  // End main page containers/setup //
+
+
+  // New game button click functionality //
+  newGameButton.addEventListener('click', playBallHandler)
+  function playBallHandler () {
+    appBody.innerHTML = ""
+    appBody.append(battersControlsContainer, pitchersControlsContainer)
+  }
+
+
+
 
   function newGame(){
     new Game().render()
   }
 
-  newGame()
+
+
+
+
+  // fetch("http://localhost:3000/api/v1/games")
+  //   .then(res => res.json())
+  //   .then(json => {
+  //     new Game(json).render()
+  //   })
 })
